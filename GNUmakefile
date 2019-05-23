8c6f0777fc04130235d3eaed2f20a28d050bc782
Lance Arsenault <lanceman@vt.edu>
2019-05-23 12:16:59 -0400

    make cleans better

--- GNUmakefile
+++ GNUmakefile
@@ -28,6 +28,8 @@ UHD_LDFLAGS :=\
 UHD_CPPFLAGS := $(shell pkg-config uhd --cflags)
 
 
+# These are the tared files that we download
+TARED_FILES := benchmark_rate.cpp benchmark_streamer.cpp gpio.cpp latency_test.cpp network_relay.cpp replay_samples_from_file.cpp rfnoc_nullsource_ce_rx.cpp rfnoc_radio_loopback.cpp rfnoc_rx_to_file.cpp rx_ascii_art_dft.cpp rx_multi_samples.cpp rx_samples_to_file.cpp rx_samples_to_udp.cpp rx_timed_samples.cpp sync_to_gps.cpp test_clock_synch.cpp test_dboard_coercion.cpp test_messages.cpp test_pps_input.cpp test_timed_commands.cpp twinrx_freq_hopping.cpp tx_bursts.cpp txrx_loopback_to_file.cpp tx_samples_from_file.cpp tx_timed_samples.cpp tx_waveforms.cpp usrp_list_sensors.cpp ascii_art_dft.hpp wavetable.hpp rx_samples_c.c tx_samples_c.c
 
 
 build: uhd.tar.gz $(BINS)
@@ -44,17 +46,26 @@ print:
 	$(CXX) $(CXXFLAGS) -c $(UHD_CPPFLAGS) $< -o $@
 
 
+
+
 $(BINS):
 	$(CXX) $(CXXFLAGS) $^ -o $@ $($@_LDFLAGS) $(UHD_LDFLAGS)
 
 
 clean:
-	rm -f *.o $(BINS)
+	rm -f *.o $(BINS) $(TARED_FILES)
+	rm -rf init_usrp/ getopt/
+
+
+# Also clean downloaded files
+cleaner: clean
+	rm -f uhd.tar.gz
 
 
 download: uhd.tar.gz
 
+
 uhd.tar.gz:
 	./getFiles.bash
-	$(MAKE)
+	@echo "You'll need to run $(MAKE) again because we just got the files we needed"
 
--- getFiles.bash
+++ getFiles.bash
@@ -1,20 +1,22 @@
 #!/bin/bash
 
+################### CONFIGURE ###############################
 UHD_TAG=v3.14.0.0
 
+# if not set it will not use the sha512 sum and tell you it
 SHA512SUM=
-
 
 SHA512SUM=05918bad6345ba4c0f\
 39a9b81d2a6d2a11634a586df71e\
 74c0be5ee03928e84ee0e96c71eb\
 13c078c63032ea33c7604e3d1bc4\
 e315508d2b72b7f990040dac09
-
+############################################################
 
 
 set -ex
 set -o pipefail
+
 
 # This gets a tarball from github.
 
@@ -28,6 +30,19 @@ function GetSrcFromGithub()
     local tag="$3"
     local tarfile=$4.tar.gz
 
+    if [ -e "$tarfile" ] && [ -n "$5" ] ; then
+        if ! echo "$5  $tarfile" | sha512sum -c ; then
+            set +x
+            echo "You have \"$tarfile\" with a bad sha512 sum"
+            exit 1
+        else
+            # success
+            set +x
+            echo "We have the tar file \"$tarfile\" already"
+            exit 0
+        fi
+    fi
+
     # This gets a tarball file from github for the package
     wget -O $tarfile https://github.com/$path/tarball/$tag
 
@@ -35,7 +50,7 @@ function GetSrcFromGithub()
         set +e
         # Check that the file has a good SHA512 sum
         if ! echo "$5  $tarfile" | sha512sum -c ; then
-            mv $tarfile $tarfile.BAD_SHA512
+            echo "You have $tarfile with a bad sha512 sum"
             exit 1 # FAIL
         fi
     else
@@ -45,6 +60,7 @@ function GetSrcFromGithub()
     # Success
 }
 
+
 GetSrcFromGithub EttusResearch uhd $UHD_TAG uhd $SHA512SUM
 
 tar -xvf uhd.tar.gz\
@@ -53,5 +69,4 @@ tar -xvf uhd.tar.gz\
  'host/examples/*.hpp'\
  'host/examples/*.c'\
  --strip-components=3
-
 
